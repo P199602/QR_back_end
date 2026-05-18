ff268818c7a9d964542f2e74a353706c3b7c5be6
Pranshu kanthali <147531908+P199602@users.noreply.github.com>
2026-05-18 20:40:39 +0530

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -1,8 +1,8 @@
 export const API_BASE =
-  import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api/"
+  import.meta.env.VITE_API_URL || "http://56.228.33.46:8000/api/"
 
 export const MEDIA_BASE =
-  import.meta.env.VITE_MEDIA_URL || "http://127.0.0.1:8000"
+  import.meta.env.VITE_MEDIA_URL || "http://56.228.33.46:8000"
 
 export const APP_BASE =
-  import.meta.env.VITE_APP_URL || "http://localhost:5173"
+  import.meta.env.VITE_APP_URL || "http://56.228.33.46:5173"
